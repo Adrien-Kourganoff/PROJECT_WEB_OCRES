e4a4cdf43d89e7d6f3d9df2db1e14ff7025e260c
AurelienDavodet <aurelien.davodet@gmail.com>
2020-11-23 12:04:27 +0100

    connexion

---
+++ frontend/src/dashboard/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+class Main extends Component {
+  
+  render () {
+    
+    return (
+      <div>
+        <div className="header">
+            <h1>Salut</h1>
+        </div>
+       
+      </div>
+    )
+  }
+}
+
+export default Main
