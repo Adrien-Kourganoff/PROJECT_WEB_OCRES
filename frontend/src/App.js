5c734e68b38ac9ca43b116f104f99bdee366a35b
SebRegn <sebastien.regnier@edu.ece.fr>
2020-11-24 15:28:26 +0100

    depart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+//import Content from './widgets';
 import './App.css';
+import Containerw1 from './widgets/widget1/containerw1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div class="widget1">
+        <Containerw1/>
+      </div>
+
     </div>
   );
 }
---
+++ frontend/src/widgets/widget1/containerw1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../index.css';
+
+export default class Containerw1 extends React.Component {
+    render() {
+        const { users, changeUser } = this.props;
+
+        return (
+            <div >
+                Annual target 
+               
+            </div>)
+    }
+}
