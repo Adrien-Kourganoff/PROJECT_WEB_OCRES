1d3e33816c0f2a898918386e04d48148b99c6564
Math1814 <49634301+Math1814@users.noreply.github.com>
2020-11-25 20:38:55 +0100

    parametre v1

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Doughnut } from "react-chartjs-2";
 import { Line } from "react-chartjs-2";
-import Logo from "./unknown.png";
-import Param from "./param.png";
-import Dash from "./dash.png";
 import Graph from "./components/Graph";
-import "./App.css";
 import DayPicker from "react-day-picker";
 import Commentaire from "./components/Commentaire/Commentaire.jsx";
+import Parameter from "./components/Parameter/Parameter.jsx"
+
+import "./App.css";
 import "react-day-picker/lib/style.css";
+
+import Logo from "./unknown.png";
+import Param from "./paramnoir.png";
+import ParamA from "./paramvert.png";
+import Dash from "./dashnoir.png";
+import DashA from "./dashrose.png";
+
 
 var option = {
   legend: {
@@ -63,142 +69,193 @@ var data2 = {
   ],
 };
 
+var id = window.location.pathname[1] === undefined ? 0 : window.location.pathname[1];
+
+
 <link
   rel="stylesheet"
   href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
 ></link>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.parameter = this.parameter.bind(this);
+    this.dashboard = this.dashboard.bind(this);
+    this.state = { page: 1 }
+  }
+  parameter() {
+    this.setState({ page: 2 });
+  }
 
-function App() {
-  console.log(window.location.pathname); //si on peut voir l'url en haut
-  var id =
-    window.location.pathname[1] === undefined ? 0 : window.location.pathname[1];
-  console.log(id);
-  return (
-    <div class="container" style={{ maxWidth: "2000px" }}>
-      <div class="row">
-        <div class="col-sm-2" style={{ fontWeight: "bold" }}>
-          <header class="App-header">
-            <div class="row">
-              <img src={Logo} style={{ margin: "auto" }}></img>
-            </div>
-            <div class="row">
-              <div class="col-sm-12">
-                <div class="choixEntre">
-                  <div class="row">
-                    <a style={{ margin: "auto" }}>Entreprise A</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row" style={{ marginTop: "40%" }}>
-              <div class="col-sm-2">
-                <img src={Dash}></img>
-              </div>
-              <div class="col-sm-10">
-                <a style={{ color: "#FDE1DE" }}>Mon DashBoard</a>
-              </div>
-            </div>
-            <div class="row" style={{ marginTop: "10%" }}>
-              <div class="col-sm-2">
-                <img src={Param}></img>
-              </div>
-              <div class="col-sm-10">
-                <a> Mes Paramètres</a>
-              </div>
-            </div>
-          </header>
-        </div>
+  dashboard() {
+    this.setState({ page: 1 });
+  }
 
-        <div class="col-sm-10" style={{ fontWeight: "bold" }}>
-          <div class="row">
-            <div class="col-sm-9">
-              <div class="row row-mid">
-                <div class="App-headerMilieu">
-                  <div class="col-sm-1"></div>
-                  <div class="col-sm-10 container-graph-benefice">
-                    <div class="row" style={{ marginTop: "1em" }}>
-                      <titre>Bénéfices</titre>
-                    </div>
-                    <div class="row row-graph-benefice">
-                      <Line data={data2} type="line" height="auto" />
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-1"></div>
-              </div>
-              <div class="row row-mid">
-                <div class="col-sm-6">
-                  <div class="App-headerMilieu">
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-10">
-                      <titre>Gains</titre>
-                      <div class="row">
-                        <Doughnut
-                          data={dataGain}
-                          options={option}
-                          height="150px"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-1"></div>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="App-headerMilieu">
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-10">
-                      <titre>Depense</titre>
-                      <div class="row">
-                        <Doughnut
-                          data={dataDep}
-                          options={option}
-                          height="150px"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-1"></div>
-                  </div>
-                </div>
-              </div>
-              <div class="row row-mid">
-                <div class="col-sm-10">
-                  <div class="App-headerMilieu">
-                    <Commentaire /> 
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-3">
+  render() {
+    return (
+      <div class="container" style={{ maxWidth: "2000px" }}>
+        <div class="row">
+          <div class="col-sm-2" style={{ fontWeight: "bold" }}>
+            <header class="App-header">
               <div class="row">
-                <div class="App-header">
-                  <div class="col-sm-1"></div>
-                  <div class="col-sm-10">
-                    <titre>Date</titre>
-                    <div class="row">
-                      <DayPicker />
-                    </div>
-                  </div>
-                  <div class="col-sm-1"></div>
-                </div>
+                <img src={Logo} style={{ margin: "auto" }}></img>
               </div>
               <div class="row">
-                <div class="App-header">
-                  <div class="col-sm-1"></div>
-                  <div class="col-sm-10">
-                    <titre>ToDo</titre>
+                <div class="col-sm-12">
+                  <div class="choixEntre">
                     <div class="row">
-                      <Graph />
+                      <a style={{ margin: "auto" }}>Entreprise A</a>
                     </div>
                   </div>
-                  <div class="col-sm-1"></div>
                 </div>
               </div>
-            </div>
+
+              {
+                this.state.page === 1
+                ?              /* btn page 1*/
+                <div>
+                  <div class="row" onClick={this.dashboard} style={{ marginTop: "40%", cursor: "pointer" }}>
+                    <div class="col-sm-2">
+                      <img src={DashA}></img>
+                    </div>
+                    <div class="col-sm-10">
+                      <a style={{ color: "#FDE1DE" }}>Mon DashBoard</a>
+                    </div>
+                  </div>
+                  
+                  <div class="row" onClick={this.parameter} style={{ marginTop: "10%", cursor: "pointer" }}>
+                    <div class="col-sm-2">
+                      <img src={Param}></img>
+                    </div>
+                    <div class="col-sm-10">
+                      <a> Mes Paramètres</a>
+                    </div>
+                  </div>
+                </div>
+                :               /*btn page 2 */
+                <div>
+                  <div class="row" onClick={this.dashboard} style={{ marginTop: "40%", cursor: "pointer" }}>
+                    <div class="col-sm-2">
+                      <img src={Dash}></img>
+                    </div>
+                    <div class="col-sm-10">
+                      <a>Mon DashBoard</a>
+                    </div>
+                  </div>
+                  <div class="row" onClick={this.parameter} style={{ marginTop: "10%", cursor: "pointer" }}>
+                    <div class="col-sm-2">
+                      <img src={ParamA}></img>
+                    </div>
+                    <div class="col-sm-10">
+                      <a style={{ color: "#78E6D0" }} > Mes Paramètres</a>
+                    </div>
+                  </div>
+                </div>
+
+              }
+            </header>
           </div>
+          
+
+          {
+            this.state.page === 1
+              /* Page 1 */
+              ? <div v-if="this.state.page == 1" class="col-sm-10" style={{ fontWeight: "bold" }}>
+                <div class="row">
+                  <div class="col-sm-9">
+                    <div class="row row-mid">
+                      <div class="App-headerMilieu">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-10 container-graph-benefice">
+                          <div class="row" style={{ marginTop: "1em" }}>
+                            <titre>Bénéfices</titre>
+                          </div>
+                          <div class="row row-graph-benefice">
+                            <Line data={data2} type="line" height="auto" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-sm-1"></div>
+                    </div>
+                    <div class="row row-mid">
+                      <div class="col-sm-6">
+                        <div class="App-headerMilieu">
+                          <div class="col-sm-1"></div>
+                          <div class="col-sm-10">
+                            <titre>Gains</titre>
+                            <div class="row">
+                              <Doughnut
+                                data={dataGain}
+                                options={option}
+                                height="150px"
+                              />
+                            </div>
+                          </div>
+                          <div class="col-sm-1"></div>
+                        </div>
+                      </div>
+                      <div class="col-sm-6">
+                        <div class="App-headerMilieu">
+                          <div class="col-sm-1"></div>
+                          <div class="col-sm-10">
+                            <titre>Depense</titre>
+                            <div class="row">
+                              <Doughnut
+                                data={dataDep}
+                                options={option}
+                                height="150px"
+                              />
+                            </div>
+                          </div>
+                          <div class="col-sm-1"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row row-mid">
+                      <div class="col-sm-10">
+                        <div class="App-headerMilieu">
+                          <Commentaire />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-3">
+                    <div class="row">
+                      <div class="App-header">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-10">
+                          <titre>Date</titre>
+                          <div class="row">
+                            <DayPicker />
+                          </div>
+                        </div>
+                        <div class="col-sm-1"></div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="App-header">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-10">
+                          <titre>ToDo</titre>
+                          <div class="row">
+                            <Graph />
+                          </div>
+                        </div>
+                        <div class="col-sm-1"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              /* Page 2 */
+              : <div className="page2">
+                <Parameter /> 
+              </div>
+          }
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ frontend/my-app/src/components/Parameter/Parameter.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "./Parameter.css";
+
+class parameter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { null:null }  
+    }
+    render() { 
+        return ( 
+            <div className="containerPage2">
+
+            </div>
+         );
+    }
+}
+ 
+export default parameter;
