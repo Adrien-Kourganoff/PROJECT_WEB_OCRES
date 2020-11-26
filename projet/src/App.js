637b3bc8f259b363f28f6dbd9e5d1af767164ac3
j0ojo <71373291+j0ojo@users.noreply.github.com>
2020-11-26 23:25:27 +0100

    jo

--- projet/src/App.js
+++ projet/src/App.js
@@ -21,6 +21,11 @@ import SideBar from './Widgets/SideBar';
   href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
   integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
   crossorigin="anonymous"
+  rel="manifest" href="%PUBLIC_URL%/manifest.json"
+	 rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico"
+ href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
+ integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" 
+ crossorigin="anonymous"
 />
 
 function App() {
--- projet/src/Widgets/SideBar.js
+++ projet/src/Widgets/SideBar.js
@@ -13,6 +13,19 @@ import './SideBar.css'
 /*import img from './Kahina_blanc.jpg';
 import logo from './logo.svg';*/
 
+<link
+  rel="stylesheet "
+ href="path/to/font-awesome/css/font-awesome.min.css"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+  crossorigin="anonymous"
+  rel="manifest" href="%PUBLIC_URL%/manifest.json"
+	 rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico"
+ href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
+ integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" 
+ crossorigin="anonymous"
+/>
+
 
 class SideBar extends React.Component{
 
@@ -35,7 +48,7 @@ class SideBar extends React.Component{
     <SideNav.Nav defaultSelected="home">
         <NavItem eventKey="home">
             <NavIcon>
-                <i className="fa fa-child" style={ { fontSize: '1.75em'}} />
+                <i className="fa fa-fw fa-home" style={ { fontSize: '1.75em'}} />
             </NavIcon>
             <NavText>
                 Admin
@@ -44,7 +57,7 @@ class SideBar extends React.Component{
 
         <NavItem eventKey="Icare">
             <NavIcon>
-            <i class="fa fa-child" aria-hidden="true" style={{ fontSize: '1.75em' }} />
+            <i class="fa fa-fw fa-home fa-5x" style={{ fontSize: '1.75em' }} ></i>
             </NavIcon>
             <NavText>
                 ICare
