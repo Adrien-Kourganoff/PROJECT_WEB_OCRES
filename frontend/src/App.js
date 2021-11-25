63d576b3755653327645b8a137f60d31241f73a5
jessicanebot2 <zac@Zaccharies-MacBook-Pro.local>
2021-11-25 12:41:09 +0100

    fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-<<<<<<< HEAD
-=======
-        
-        <img src={logo} className="App-logo" alt="logo" />
->>>>>>> 57d13e43c8b803801706ba6d23dc213667e1756a
-        <p>
-          Edit <code>src/App.js</code> and save to reload. teeeeest?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
