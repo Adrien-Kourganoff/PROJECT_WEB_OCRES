945de0319d826af0705019c4741ecab74ed66eff
ChaiseLongue <57494281+ChaiseLongue@users.noreply.github.com>
2021-11-26 11:44:13 +0100

    Add map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Map from './map';
+
 function App() {
   return (
-    <div className="App">
+    <Map/>
+  );
+}
+
+export default App;
+
+/*
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +30,4 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+*/
