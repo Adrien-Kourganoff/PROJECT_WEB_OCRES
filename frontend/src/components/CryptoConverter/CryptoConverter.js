99ad628dc106bc6f07c57e27e4eaae73f69fa7da
dan-555 <61452094+dan-555@users.noreply.github.com>
2021-11-28 14:47:13 +0100

    converter
    finish

--- frontend/src/components/CryptoConverter/CryptoConverter.js
+++ frontend/src/components/CryptoConverter/CryptoConverter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import  {Card ,ListGroup,Dropdown,InputGroup,DropdownButton,FormControl} from 'react-bootstrap';
 import CryptoConverterStyle from './CryptoConverter.module.css';
 import classNames from 'classnames';
@@ -6,32 +6,113 @@ import classNames from 'classnames';
 export default function CryptoConverter(props) {
 
 
+    const [index,setIndex]=useState(0);
+
+    const [CurrencyRefIndex,setCurrencyRefIndex]=useState(1);
+    const [CurrencyRefName,setCurrencyRefName]=useState(props.data[1].name);
+    const [CurrencyRefSymbol,setCurrencyRefSymbol]=useState(props.data[1].symbol);
+    const [CurrencyRefImage,setCurrencyRefImage]=useState(props.data[1].image);
+    const [CurrencyRefPrice,setCurrencyRefPrice]=useState(props.data[1].current_price);
+
+
+  useEffect(() => {
+
+    calculInit();
+            
+  },[]);
+
+
+
+    const handleSelect=(e)=>{
+        setIndex(e);
+        console.log(e);
+        document.getElementById("titleSelector").src=props.data[e].image;
+      }
+
+      function calculInit()
+      {
+        document.getElementById('montantaAConvertir').value=1;
+
+        //Convertir devise ref en dollar
+        var refCurrencyToDollar = 1*CurrencyRefPrice;
+
+        //Convertir dollar en devise souhaitée
+        var DollarToRefCurrency = refCurrencyToDollar/props.data[index].current_price;
+        DollarToRefCurrency.toFixed(3);
+        document.getElementById('montantConverti').value=DollarToRefCurrency.toFixed(3);
+      }
+
+    function calcul()
+    {
+            var montantAconvertir,
+            element = document.getElementById('montantaAConvertir');
+
+            if (element != null) {               
+                montantAconvertir = element.value; 
+
+               //Convertir devise ref en dollar
+               var refCurrencyToDollar = montantAconvertir*CurrencyRefPrice;
+              // console.log(refCurrencyToDollar);
+           
+               //Convertir dollar en devise souhaitée
+               var DollarToRefCurrency = refCurrencyToDollar/props.data[index].current_price;
+               DollarToRefCurrency.toFixed(3);
+
+                console.log(DollarToRefCurrency.toFixed(3));
+
+                document.getElementById('montantConverti').value=DollarToRefCurrency.toFixed(3);
+            }
+            else {
+                montantAconvertir = null;
+            }
+    }
     return( 
             <Card className ="h100" >
                     <ListGroup variant="flush">
                         <ListGroup.Item>
                         
-                        <div></div>
-       <div>{props.data[1].name}, {props.data[1].symbol.toUpperCase()}</div>             
+       <div>{CurrencyRefName}, {CurrencyRefSymbol.toUpperCase()}</div>             
                         <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1"><img src={props.data[1].image} alt={props.data[1].symbol} className={classNames(CryptoConverterStyle.logoSize)}/></InputGroup.Text>
-                             <FormControl placeholder="currency" aria-label="currency" aria-describedby="currency"/>
+                        <InputGroup.Text id="basic-addon1"><img src={CurrencyRefImage} alt={CurrencyRefSymbol} className={classNames(CryptoConverterStyle.logoSize)}/></InputGroup.Text>
+                        
+                        <FormControl id="montantaAConvertir" type="number" aria-label="currency" aria-describedby="currency"/>
+                       
                         </InputGroup>
                         
                     <div align="center">Vers</div> 
-                    <div className={classNames(CryptoConverterStyle.marginStyle)}>Ethereum, ETH</div>
-                    <InputGroup className={classNames("mb-3")}>
-                        <DropdownButton variant="outline-secondary" title="Dropdown" id="input-group-dropdown-1">
-                                <Dropdown.Item href="#">Action</Dropdown.Item>
-                                <Dropdown.Item href="#">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                        </DropdownButton>
-                        <FormControl aria-label="Text input with dropdown button" />
-                        </InputGroup>                    
+                   
+                    <div className={classNames(CryptoConverterStyle.marginStyle)}>{props.data[index].name}, {props.data[index].symbol.toUpperCase()}</div>
+                   
+                    <InputGroup className="mb-3">
+                  
+                    <DropdownButton       onSelect={handleSelect}
+ aria-labelledby="drop4" id="currencySelector" className="custom-colorBtn" title={<img id={"titleSelector"} src={props.data[0].image} alt={props.data[0].symbol} className={classNames(CryptoConverterStyle.logoSize)}/>} id="input-group-dropdown-1"
+                    >
+                         {Array.from({length: props.data.length}).map((crypto,index) => (
+                           
+                           <Dropdown.Item className={classNames("drop" + index)} eventKey={index} ><img id={"indexIMG" + index} src={props.data[index].image} alt={props.data[1].symbol} className={classNames(CryptoConverterStyle.logoSize)}/>&nbsp;{props.data[index].name}</Dropdown.Item>
+                         ))}
+                    </DropdownButton>
+                    <FormControl value={"0.000"} id="montantConverti" disabled aria-label="Text input with dropdown button" />
+                   
+                    <div className="container">
+        <div className="col-md-12 text-center">
+            <button className={classNames("btn",CryptoConverterStyle.btnStyle)} type="button"  onClick={calcul} >Convertir</button>
+        </div>
+    </div>
+                </InputGroup>
+                    
+                    
+                    
+                    
+                    
+                    
+                                    
 
                         </ListGroup.Item>
                     </ListGroup>
 
             </Card>
         )
+
 }
