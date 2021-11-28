dd41a609ddbac29212965e1eed5ec7e03a602223
Margot Lutic <margot.lutic3@gmail.com>
2021-11-28 23:31:56 +0100

    home+beau

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Weather App</h1>
-            <Link to="/map">Dashboard</Link>
+        <div class='title'>
+            <h1>Notre application météo</h1>
+            <Link to="/map">Pour accéder à la météo, cliquer ici</Link>
         </div>
     );
 };
