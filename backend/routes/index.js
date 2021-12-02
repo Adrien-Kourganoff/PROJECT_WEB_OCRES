d9ecdffe79563cbb84eb4d36671182396ff46e7c
Margot Lutic <margot.lutic3@gmail.com>
2021-12-02 12:39:30 +0100

    test_connexion_mongo

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,35 @@
+const mongoose = require('mongoose');
 var express = require("express");
 var router = express.Router();
 
+mongoose.Promise = global.Promise;
+//const dbName = "Projet_web_ocres_S1";
+const dbURL = 'mongodb://localhost:27017/Projet_web_ocres_S1';
+
+function addData(data){
+  mongoose.connect(dbURL, {useNewUrlParser: true});
+  mongoose.Collection('weather').insertMany(data);
+};
+
+function takeData(){
+  mongoose.connect(dbURL, {useNewUrlParser: true});
+  let dataWeather = mongoose.weather.find({weather});
+  let dataDeg = mongoose.weather.find({deg});
+  let dataWind = mongoose.weather.find({wind});
+  let dataHumidity = mongoose.weather.find({humidity});
+}
+
 /* GET home page. */
+
 router.get("/", function(req, res, next) {
   res.send("This is my homepage");
+  let test = [
+    {weather : "sun", deg : 15, wind : 29, humidity :15},
+    {weather : "snow", deg : 5, wind : 20, humidity :40},
+    {weather : "snow", deg : 2, wind : 30, humidity :50},
+    {weather : "rain", deg : 7, wind : 23, humidity :90},
+  ]
+  addData(test);
 });
 
 module.exports = router;
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -5,6 +5,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div class='title'>
+            <img src="nuage.png" style={{width:50, height:50}} alt="" />
             <h1>Notre application météo</h1>
             <Link to="/map">Pour accéder à la météo, cliquer ici</Link>
         </div>
