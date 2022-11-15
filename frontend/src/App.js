cf11830a30ee07c711b0d4df9b434b1bf7e05c0f
hanabens <94543855+hanabens@users.noreply.github.com>
2022-11-15 09:05:10 +0100

    modif app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,14 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< Updated upstream
     <div className="App">
       Bienvenue sur SongBox
 
       {/*afficher l'horloge  */}
-=======
-    <div className="Accueil">
-      <t1>Bienvenue sur SongBox</t1>
->>>>>>> Stashed changes
       <Horloge />
       <Welcome />
     </div>
   );
 }
 
-export default App;
+export default App
