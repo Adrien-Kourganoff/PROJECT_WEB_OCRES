9d24d32ad1c168ab3fac2723280412c212773f5d
Ryan Baazia <baaziar@gmail.com>
2022-11-25 19:21:36 +0100

    Base 1

---
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className='Sidebar'>
+
+            <div className='Logo'>
+                <span>
+                    DALY <span>PAPER.</span>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ frontend/src/Components/Topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Topbar.css'
+
+const Topbar = () => {
+    return (
+        <div className='Topbar'>
+
+            <div className='Date'>
+                Bonjour, aujourd'hui nous sommes le
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
