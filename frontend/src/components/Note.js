60557f31c22599700ddd6ffac051b92604d22516
hanabens <94543855+hanabens@users.noreply.github.com>
2022-11-26 18:48:33 +0100

    notes

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,9 +1,23 @@
-const Note = () => {
-  return (
-    <div>
-      <h1>Notes Page</h1>
-    </div>
-  );
-};
+import React from "react";
+
+
+import "./note.css";
+
+class Note extends React.Component {
+  render() {
+    return (
+      <div>
+
+        <div className="Bienvenue">Votre Dashboard, Charlie</div>
+        <div className="titre_notes">Notes</div>
+        <div className="rectangle" />
+        <div className="Ajouter"> <input className="Ajouter" type="submit" value="Ajouter une note" /></div>
+
+      </div>
+    );
+  }
+}
+
+
 
 export default Note;
