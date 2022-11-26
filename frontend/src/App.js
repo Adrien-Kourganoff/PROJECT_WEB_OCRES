bd90b7076042bb12c514055491d9764a9db6f52c
Joshua Ekollo <78730734+joshua87000@users.noreply.github.com>
2022-11-26 13:48:05 +0100

    Date + Bouton(sans style)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Topbar from './Components/Topbar/Topbar';
+import DashBoard from './Components/DashBoard/DashBoard'
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Reglage from './Components/Reglage/Reglage';
+
+
+
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+       <Routes>
+        <Route  path ="/" element = {<DashBoard />} />
+        <Route  path ="/reglage" element = {<Reglage />} />
+       </Routes>
+    </BrowserRouter>
+
+
+
+    /*<div className="App">
       <div className="AppGlass">
         <Sidebar />
         <Topbar />
       </div>
 
-    </div>
+    </div>*/
   );
 }
 
--- frontend/src/Components/Topbar/Topbar.jsx
+++ frontend/src/Components/Topbar/Topbar.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './Topbar.css'
 
 const Topbar = () => {
+
+    var today = new Date();
+    const mois = ["Janvier" , "Fevrier" , "Mars" , "Avril" , "Mai" , "Juin" , "Juiller", "Aout" , "Septembre" , "Octobre" , "Novembre", "Decembre"]
+    // const oui = 12
     return (
         <div className='Topbar'>
 
             <div className='Date'>
-                Bonjour, aujourd'hui nous sommes le
+                Bonjour, aujourd'hui nous sommes le {today.getDate()} {mois[today.getMonth()]} {today.getFullYear()}
             </div>
         </div>
     )
