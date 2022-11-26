7a9dd100ea21c7998070442c60bbcb4252d52315
Audrey Berthelot <audrey.berthelot@edu.ece.fr>
2022-11-26 14:13:54 +0100

    feat: Added notes Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Horloge from "./components/horloge";
 import Welcome from "./components/welcome";
 import { Routes, Route } from "react-router-dom";
+import Note from "./components/Note";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
-      <Route path="/notes" element={<Notes />} />
+      <Route path="/notes" element={<Note />} />
       <Route path="/horloge" element={<Horloge />} />
     </Routes>
   );
