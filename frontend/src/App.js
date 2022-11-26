ca4df45703194b23674f1c68192e595f1a30538b
Audrey Berthelot <audrey.berthelot@edu.ece.fr>
2022-11-26 14:10:07 +0100

    feat: Added notes Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
+      <Route path="/notes" element={<Notes />} />
       <Route path="/horloge" element={<Horloge />} />
     </Routes>
   );
---
+++ frontend/src/components/Note.js
@@ -0,0 +1,9 @@
+const Note = () => {
+  return (
+    <div>
+      <h1>Notes Page</h1>
+    </div>
+  );
+};
+
+export default Note;
