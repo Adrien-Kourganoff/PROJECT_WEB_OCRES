c7a40f750df136c53caada089b93f3a08c694c42
hanabens <94543855+hanabens@users.noreply.github.com>
2022-11-28 16:56:14 +0100

    font de la page d'acceuil

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -9,8 +9,14 @@ class Note extends React.Component {
       <div>
 
         <div className="Bienvenue">Votre Dashboard, Charlie</div>
+
+
         <div className="titre_notes">Notes</div>
+
+
+
         <div className="rectangle" />
+
         <div className="Ajouter"> <input className="Ajouter" type="submit" value="Ajouter une note" /></div>
 
       </div>
